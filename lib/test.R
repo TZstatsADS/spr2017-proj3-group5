aa33f21d08d83ec7bd9995ee0235815c55c4b280
jz2773 <jz2773@columbia.edu>
2017-03-26 18:11:47 -0400

    Add files via upload

--- lib/test.R
+++ lib/test.R
@@ -5,22 +5,32 @@
 ### Author: Yuting Ma
 ### Project 3
 ### ADS Spring 2016
+### 
+### Fit the classfication model with testing data
 
-test <- function(fit_train, dat_test){
+### Input: 
+###  - the fitted classification model using training data
+###  -  processed features from testing images 
+### Output: training model specification
+
+### load libraries
+
+fit_test <- function(test_gbm=sift_test, test_svm=hog_test, label=label_test){
+
+    gbmtest_pre <- predict(fit_gbm2,test_gbm,n.tree=best_ntree,type="response")
+    test_tmg <-system.time(gbmtest_pre <- predict(fit_gbm2,test_gbm,n.tree=best_ntree,type="response"))
+    gbm_error<-mean(gbmtest_pre!=label)
+    
+    svmtest_pre <- predict(fit_svm2, newdata=test_svm, gamma=best_gamma,cost=best_cost,type="response")
+    test_tms<-system.time(svmtest_pre <- predict(fit_svm2, newdata=test_svm, gamma=best_gamma,cost=best_cost,type="response"))
+    svm_error<-mean(svmtest_pre!=label)
+    
+    return(c(test_tmg,gbm_error,test_tms,svm_error))
+    
+    
+  }
+    
   
-  ### Fit the classfication model with testing data
   
-  ### Input: 
-  ###  - the fitted classification model using training data
-  ###  -  processed features from testing images 
-  ### Output: training model specification
-  
-  ### load libraries
-  library("gbm")
-  
-  pred <- predict(fit_train$fit, newdata=dat_test, 
-                  n.trees=fit_train$iter, type="response")
-  
-  return(as.numeric(pred> 0.5))
-}
+ 
 
--- lib/train.R
+++ lib/train.R
@@ -7,7 +7,7 @@
 ### ADS Spring 2016
 
 
-train <- function(dat_train, label_train, par=NULL){
+fit_train <- function(best_ntree, best_gamma, best_cost){
   
   ### Train a Gradient Boosting Model (GBM) using processed features from training images
   
@@ -16,22 +16,33 @@ train <- function(dat_train, label_train, par=NULL){
   ###  -  class labels for training images
   ### Output: training model specification
   
-  ### load libraries
-  library("gbm")
-  
+
   ### Train with gradient boosting model
-  if(is.null(par)){
-    depth <- 3
-  } else {
-    depth <- par$depth
-  }
-  fit_gbm <- gbm.fit(x=dat_train, y=label_train,
-                     n.trees=2000,
+  
+  gbm_train_time<-system.time(gbm.fit(x=sift_train, y=label_train,
+                     n.trees=best_ntree,
+                     shrinkage=.001,
+                     n.minobsinnode=20,
                      distribution="bernoulli",
                      interaction.depth=depth, 
                      bag.fraction = 0.5,
-                     verbose=FALSE)
-  best_iter <- gbm.perf(fit_gbm, method="OOB", plot.it = FALSE)
-
-  return(list(fit=fit_gbm, iter=best_iter))
+                     verbose=FALSE))
+  fit_gbm2 <- gbm.fit(x=sift_train, y=label_train,
+                       n.trees=best_ntree,
+                       shrinkage=.001,
+                       n.minobsinnode=20,
+                       distribution="bernoulli",
+                       interaction.depth=depth, 
+                       bag.fraction = 0.5,
+                       verbose=FALSE)
+  best_iter <- gbm.perf(fit_gbm233, method="OOB", plot.it = FALSE)
+  
+  
+  ##train svm model(advanced)
+  fit_svm2 <- svm(label_train~., data=hog_train_new, kernel="radial",scale=TRUE,cost=best_cost,gamma=best_gamma)
+  svm_train_time<-system.time(svm(label_train~., data=hog_train_new, kernel="radial",scale=TRUE,cost=best_cost,gamma=best_gamma))
+  
+  return(list(fit_gbm2, fit_svm2, svm_train_time, gbm_train_time))
+  
+  
 }
